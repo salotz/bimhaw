6598551fd850ef967a4b331ac37ade304b733fa7
Samuel Lotz <samuel.lotz@salotz.info>
2019-12-21 23:51:08 -0500

    update of readme

---
+++ docs/example_config.py
@@ -0,0 +1,135 @@
+PROFILES = [
+  'bimhaw',
+  'common',
+]
+
+# POSIX shell (whatever that is)
+SH_LOGIN_ENVS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+
+
+SH_NONLOGIN_ENVS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+
+# scripts to run on logout
+SH_LOGOUTS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+
+}
+
+
+# salotz.sh_aliases
+SH_ALIASES = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+
+}
+
+
+# salotz.sh_funcs
+SH_FUNCS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+
+}
+
+
+SH_PROMPT = {
+  'bimhaw' : 'default',
+  'common' : 'default',
+}
+
+BASH_PROMPT = {
+  'bimhaw' : 'default',
+  'common' : 'default',
+}
+
+BASH_LOGIN_SH_ENVS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+BASH_LOGIN_BASH_ENVS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+
+BASH_NONLOGIN_ENVS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+BASH_INTERACTIVE_ENVS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+BASH_ALIASES = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+BASH_FUNCS = {
+
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
+
+BASH_AUTOCOMPLETIONS = {
+  'bimhaw' : [
+  ],
+
+  'common' : [
+  ],
+}
