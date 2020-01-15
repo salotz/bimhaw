7b70685d1ea96eb4d531b1ee8484d1320e4a0eae
Samuel Lotz <samuel.lotz@salotz.info>
2020-01-15 17:16:12 -0500

    not much, don't want to include the execution stuff anymore
    Maybe I will allow it to integrate and compose with other invoke
    interfacs but for now no

--- src/bimhaw/main.py
+++ src/bimhaw/main.py
@@ -82,7 +82,7 @@ def profile_ls(cx):
 profile_ns.add_task(profile_ls, name='ls')
 
 @task
-def ls(cx, name):
+def lib(cx, name):
 
     bimhaw_p = Path(osp.expandvars("$BIMHAW_DIR"))
 
@@ -91,75 +91,18 @@ def ls(cx, name):
 
     print('\n'.join(mods))
 
+@task
+def libs(cx):
 
-## Setups
-
-# modules that get run once per environment (.e.g on new install of
-# OS) such as installing packages or setting up directories etc.
-
-
-
-# @task()
-# def setups_ls(cx):
-
-#     setups = list_modules('setups')
-
-#     print('\n'.join(setups))
-
-
-# setups_ns = Collection()
-# setups_ns.add_task(setups_ls, name='ls')
-# setups_ns.add_task(setups_run, name='run')
-
-# ## Inits
-
-# # modules that get run per session but manually so
-
-# inits_ns = Collection()
-# inits_ns.add_task(inits_ls, name='ls')
-# inits_ns.add_task(inits_run, name='run')
-
-# ## Bins
-
-# # executables that are callable from bimhaw. Can use to organize those
-# # scripts in the "misc" folder.
-
-# bins_ns = Collection()
-# bins_ns.add_task(bin_ls, name='ls')
-# bins_ns.add_task(bin_run, name='run')
-
-# ## Check ins/outs
-
-# # scripts to run for different working sets "wsets" for when you
-# # arrive at a specific machine. Useful for keeping a large set of git
-# # repos in sync or a sneaker-net of updating your computer.
-
-# checkins_ns = Collection()
-# checkins_ns.add_task(checkins_ls, name='ls')
-# checkins_ns.add_task(checkins_run, name='run')
-
-# checkouts_ns = Collection()
-# checkouts_ns.add_task(checkouts_ls, name='ls')
-# checkouts_ns.add_task(checkouts_run, name='run')
-
-
-# ## Configs
-
-# # configurations, just able to show which ones are availabl
-# configs_ns = Collection()
-# configs_ns.add_task(configs_ls, name='ls')
-# configs_ns.add_task(configs_run, name='path')
-
-
-# ## Resources
-
-# # not configuration, but pure and utter data. For resources scripts in
-# # any other module might rely on
-# resources_ns = Collection()
-# resources_ns.add_task(resources_ls, name='ls')
-# resources_ns.add_task(resources_run, name='path')
-
-
+    mods = ['setups',
+            'inits',
+            'bin',
+            'checkings',
+            'checkouts',
+            'configs',
+            'resources',
+    ]
+    print('\n'.join(mods))
 
 
 ## Top level
@@ -167,16 +110,9 @@ main_ns = Collection()
 
 # namespaces
 main_ns.add_collection(profile_ns, name='profile')
-# main_ns.add_collection(setups_ns, name='setup')
-# main_ns.add_collection(inits_ns, name='init')
-# main_ns.add_collection(bins_ns, name='run')
-# main_ns.add_collection(checkins_ns, name='checkin')
-# main_ns.add_collection(checkouts_ns, name='checkout')
-# main_ns.add_collection(configs_ns, name='profile')
-# main_ns.add_collection(resources_ns, name='profile')
 
 # individual tasks
-tasks = [link_shells, current, ls]
+tasks = [link_shells, current, lib, libs]
 for task in tasks:
     main_ns.add_task(task)
 
